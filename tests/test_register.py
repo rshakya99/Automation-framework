0fe629ebb28d90681495a8d58dd43d2a10fd0165
Rohit shakya <46860617+rshakya99@users.noreply.github.com>
2025-09-04 16:24:58 +0530

    Update test_register.py

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,6 +1,7 @@
-import time, random
+import random
 import allure
 from pages.register_page import RegisterPage
+
 
 @allure.feature("User Registration")
 @allure.story("Register with new user")
@@ -9,10 +10,11 @@ def test_register_new_user(driver):
     with allure.step("Open registration page"):
         page.open()
     with allure.step("Register with unique email"):
-        unique_email = f"user_{random.randint(1,1000)}@yopmail.com"
+        unique_email = f"user_{random.randint(1,10000)}@yopmail.com"
         page.register(unique_email, "V9!mQ7$rLp2@tX")
     with allure.step("Verify registration success"):
         assert page.is_registration_successful()
+
 
 @allure.feature("User Registration")
 @allure.story("Register with existing email")
